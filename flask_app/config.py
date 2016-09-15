06488979865c013e1a59342c370ead286606a4a0
Mikhail Ivanov <mivanov@mirantis.com>
2016-09-15 14:49:18 +0300

    Update

--- flask_app/config.py
+++ flask_app/config.py
@@ -2,7 +2,7 @@ import os
 
 #MONGODB_HOST = os.getenv("MONGODB_PORT_27017_TCP_ADDR", "localhost")
 #MONGODB_PORT = int(os.getenv("MONGODB_PORT_27017_TCP_PORT", "27017"))
-#MONGODB_DB = "pkgdb"
-SECRET_KEY = "DO NOT USE THIS IN PRODUCTION!"
+#MONGODB_DB = ""
+SECRET_KEY = "Secrets"
 ADMIN_USERNAME = "admin"
 ADMIN_PASSWORD = "admin"
